4cc70c42304eae20805bc9959be4e1ffbd133712
danrobuilds <84203730+danrobuilds@users.noreply.github.com>
2023-11-27 20:11:51 +0000

    hi

--- main/app.py
+++ main/app.py
@@ -2,6 +2,14 @@ from flask import Flask, flash, redirect, render_template, request, session
 
 app = Flask(__name__)
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route("/")
 def index():
     return render_template("index.html")
