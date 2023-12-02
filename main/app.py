e0cbff31ca7ed04896209717dab98dddd1007486
dylangbruckner <144275552+dylangbruckner@users.noreply.github.com>
2023-12-02 05:53:55 +0000

    should be a working scraper

--- main/app.py
+++ main/app.py
@@ -18,6 +18,35 @@ today = datetime.date.today()
 time = datetime.time
 fdate = f"{today.month}-{today.day}-{today.year}";
 
+def lundinentree(samplehtml):
+    # finds specific section of the code
+    start_tag = samplehtml.find(lambda tag: tag.name == 'tr' and 'Entrees' in tag.get_text())
+    end_tag = samplehtml.find(lambda tag: tag.name == 'tr' and 'Veg,Vegan' in tag.get_text())
+
+    # this should (theoretically) add all of the entrees to a list called entrees
+    if start_tag and end_tag:
+        content_set = set()
+        current_tag = start_tag.find_next()
+    
+        while current_tag and current_tag != end_tag:
+            content_set.add(str(current_tag))
+            current_tag = current_tag.find_next()
+
+        content_between = ''.join(content_set)
+
+        soup_between = BeautifulSoup(content_between, 'html.parser')
+        entrees_tags = soup_between.find_all("a")
+
+        # Extract text from each <a> tag and store in the entrees list
+        unique_entrees = set(tag.get_text(strip=True) for tag in entrees_tags)
+    
+        # Convert the set to a list if needed
+        entrees = list(unique_entrees)
+
+        return entrees
+    else:
+        return("no entrees found")
+
 # Scraper
 BREAKFAST = f"https://www.foodpro.huds.harvard.edu/foodpro/menu_items.asp?date={fdate}&type=30&meal=0"
 LUNCH = f"https://www.foodpro.huds.harvard.edu/foodpro/menu_items.asp?date={fdate}&type=30&meal=1"
@@ -33,27 +62,7 @@ brehtml = BeautifulSoup(breakfast.content, "html.parser")
 lunhtml = BeautifulSoup(lunch.content, "html.parser")
 dinhtml = BeautifulSoup(dinner.content, "html.parser")
 
-# finds specific section of the code
-start_tag = dinhtml.find("tr", text_="Entrees")
-end_tag = dinhtml.find("tr", text_="Veg,Vegan")
 
-# this should (theoretically) add all of the entrees to a list called entrees
-if start_tag and end_tag:
-    content_between = ''
-    current_tag = start_tag.find_next()
-    while current_tag and current_tag != end_tag:
-        content_between += str(current_tag)
-        current_tag = current_tag.find_next()
-
-    soup_between = BeautifulSoup(content_between, 'html.parser')
-    entrees_tags = soup_between.find_all("a")
-
-    # Extract text from each <a> tag and store in the entrees list
-    entrees = [tag.get_text(strip=True) for tag in entrees_tags]
-
-    print("Entrees:", entrees)
-else:
-    print("Start or end tag not found.")
 
 
 @app.after_request
